1e6c812550e2e4d30587b5782a6e1d3d4a5abacd
Sebastian Neumair <sebastianneumair@basti.fritz.box>
2020-11-13 10:00:58 +0100

    Add named outlet to display Modal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ModalComponent } from './components/modal';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'modal',
+  outlet: 'modal',
+  component: ModalComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/modal/modal.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ModalComponent {
+  constructor(private router: Router) { }
+
+  public close(): void {
+    this.router.navigate([{outlets: { modal: null }}]);
+  }
+}
